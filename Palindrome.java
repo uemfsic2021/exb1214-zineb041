0108283012a4d83715b045eae6a092273b34113a
zineb041 <>
2021-04-05 22:50:19 +0000

    ca marche pour moi

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,17 @@
 public class Palindrome{
     public static boolean is_palindrome(String s)
     {
-        // add your code here
-        return true;
+        int i=0;
+        int j=s.length()-1;
+        while((i<j)&&(s.charAt(i)==s.charAt(j))){
+          i++;
+          j--;
+        }
+        if(s.charAt(i)!=s.charAt(j)){
+          return false;
+        }
+        else{
+          return true;
+        }
     }
 }
